62c7e268189be1871e6bd53fdcea78079c3f437a
Era Andriandalaoarivony <era@mail.com>
2024-10-15 15:31:25 +0200

    ajout du serveur de front

---
+++ frontend/server.js
@@ -0,0 +1,19 @@
+// server.js
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000; // Vous pouvez changer le port si nécessaire
+
+// Servir les fichiers statiques (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname)));
+
+// Point d'entrée pour la page d'accueil
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Démarrer le serveur
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
